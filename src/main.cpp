9f75fcb400f06655dda59ec129de10c082f88bfe
Nafih-SA <nafih@softwareassociates.in>
2021-02-03 14:53:58 +0530

    Add Ble server multiconnect

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,108 @@
 #include <Arduino.h>
-
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLE2902.h>
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define BatteryService (uint16_t)0x180F
+#define BatteryChara (uint16_t)0x2A19
+
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define CHARA_UUID2 "4b69b0fe-653f-11eb-ae93-0242ac130002"
+
+uint8_t level = 57;
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+    BLEDevice::startAdvertising();
+  };
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+  }
+};
+
+
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  Serial.println("Starting BLE Server");
 
   BLEDevice::init("ESP32_BLE_Test");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  /********** Service 1 **************/
+  BLEService *pService1 = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService1->createCharacteristic(
       CHARACTERISTIC_UUID,
       BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE);
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
 
-  pCharacteristic->setValue("Hello World says Neil");
+  // Create a BLE Descriptor
+  pCharacteristic->addDescriptor(new BLE2902());
 
-  
-  BLECharacteristic *pCharacteristic2 = pService->createCharacteristic(
-      CHARA_UUID2,
-      BLECharacteristic::PROPERTY_READ |
-          BLECharacteristic::PROPERTY_WRITE);
+  // Start the service
+  pService1->start();
 
-  pCharacteristic2->setValue("Hi World says Nafih");
+  /********** Battery Service **************/
+  // BLEService *pService2 = pServer->createService(BatteryService); //Declared Globally
+  // BLECharacteristic *pCharacteristicBatt = pService2->createCharacteristic(BatteryChara, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+  // pCharacteristicBatt->setValue("Percentage 0 - 100");
+  // pCharacteristicBatt->addDescriptor(&BatteryLevelDescriptor);
+  // pCharacteristicBatt->addDescriptor(new BLE2902());
+  // pService2->start();
 
-  pService->start();
-  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0); // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
   Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  delay(2000);
+
+  if (deviceConnected)
+  {
+    pCharacteristic->setValue((uint8_t*)&value, 4);
+        pCharacteristic->notify();
+        value++;
+        delay(100); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+  }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+
+  delay(1000);
 }
